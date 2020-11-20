2f79b8902e204aa427aaaa4c385e73ae81798e89
DominickDeJesus <dominick.dejesus@gmail.com>
2020-11-19 22:21:11 -0500

    added some example code

---
+++ pages/[pageId].jsx
@@ -0,0 +1,72 @@
+
+import React from 'react'
+import Head from 'next/head'
+
+import { getPageTitle, getAllPagesInSpace } from 'notion-utils'
+import { NotionAPI } from 'notion-client'
+import { NotionRenderer } from 'react-notion-x'
+import "react-notion-x/src/styles.css";
+
+// used for code syntax highlighting (optional)
+import "prismjs/themes/prism-tomorrow.css";
+
+// used for collection views (optional)
+import "rc-dropdown/assets/index.css";
+
+// used for rendering equations (optional)
+import "katex/dist/katex.min.css";
+const notion = new NotionAPI()
+
+export const getStaticProps = async (context) => {
+  const pageId = context.params.pageId
+  const recordMap = await notion.getPage(pageId)
+
+  return {
+    props: {
+      recordMap
+    },
+    revalidate: 10
+  }
+}
+
+export async function getStaticPaths() {
+  const rootNotionPageId = '067dd719a912471ea9a3ac10710e7fdf'
+  const rootNotionSpaceId = 'fde5ac74-eea3-4527-8f00-4482710e1af3'
+
+  // This crawls all public pages starting from the given root page in order
+  // for next.js to pre-generate all pages via static site generation (SSG).
+  // This is a useful optimization but not necessary; you could just as easily
+  // set paths to an empty array to not pre-generate any pages at build time.
+  const pages = await getAllPagesInSpace(
+    rootNotionPageId,
+    rootNotionSpaceId,
+    notion.getPage.bind(notion)
+  )
+
+  const paths = Object.keys(pages).map((pageId) => `/${pageId}`)
+
+  return {
+    paths,
+    fallback: true
+  }
+}
+
+export default function NotionPage({ recordMap }) {
+  if (!recordMap) {
+    return null
+  }
+
+  const title = getPageTitle(recordMap)
+  console.log(title, recordMap)
+
+  return (
+    <>
+      <Head>
+        <meta name='description' content='React Notion X demo renderer.' />
+        <title>{title}</title>
+      </Head>
+
+      <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
+    </>
+  )
+}
